caf83ae465bdf284a24c348dd8029d1e61ca0b93
Ruben Garcia <1998.ruben26@gmail.com>
2024-11-10 13:51:25 -0600

    Token validation

--- fastapi_app/endpoints/authorization_code.py
+++ fastapi_app/endpoints/authorization_code.py
@@ -1,11 +1,13 @@
 
 from fastapi_app.endpoints import router
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 import logging
 import httpx
 import os 
 import json
+import jwt
+from jwt import PyJWKClient
 
 logger = logging.getLogger(__name__)
 
@@ -13,6 +15,9 @@ cognito_domain = "https://autofactura.auth.us-east-1.amazoncognito.com"
 client_id = "5g70h8hdc94pfll9vq42n7pons"
 client_secret = "qcdjhu4u5ab7b5icraiidtt4hpfa6ps3sidecojsi1b4da8i3av"
 redirect_uri = "http://localhost:8000/callback"
+jwks_url = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_RSQXxoYER/.well-known/jwks.json"
+region = "us-east-1"
+user_pool_id = "us-east-1_RSQXxoYER"
 
 @router.get("/callback")
 async def callback(request: Request):
@@ -23,7 +28,12 @@ async def callback(request: Request):
     
     tokens = await exchange_code_for_tokens(authorization_code)
     
-    return tokens
+    # Validate the ID token.
+    id_token = tokens.get("id_token")
+    if not id_token or not await validate_jwt(id_token):
+        raise HTTPException(status_code=401, detail="Invalid or expired ID token")
+    
+    return "bien hecho"
 
 async def exchange_code_for_tokens(code:str):
     url = f"{cognito_domain}/oauth2/token"
@@ -52,3 +62,26 @@ async def exchange_code_for_tokens(code:str):
             }
         else:
             return {"error": response_data.get("error","Token exchange failed")}
+        
+async def validate_jwt(token:str) -> bool:
+    # Crear un cliente PyJWK para obtener la clave pública
+    jwks_client = PyJWKClient(jwks_url)
+    
+    try:
+        #Get the token header and find the corresponding key.
+        signing_key = jwks_client.get_signing_key_from_jwt(token)
+        
+        #Decode and validate token.
+        decoded_token = jwt.decode(
+            token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience = client_id
+        )
+        return True
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
