39c0733348ad03c40846371fdb91dbddb164e7d4
Juan Antonio Aramburo Pasapera <ja.arpa97@gmail.com>
2024-11-22 18:41:52 -0600

    Middle ware files in left in Juan computer by Ruben

---
+++ fastapi_app/middleware/middleware.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Request
+from fastapi_app.application import app
+
+
+@app.middleware("http")
+async def validate_jwt(self, token: str) -> dict:
+    try:
+        # We use the same instance of jwks_client
+        signing_key = await self.get_signing_key(token)
+
+        decoded_token = decode(
+            token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience=self.client_id,
+            issuer=self.issuer,
+        )
+        return decoded_token
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    except Exception as e:
+        print(f"Unexpected error during JWT validation: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+async def get_signing_key(self, token: str):
+    """
+    Obtain the public key from the PyJWK client.
+    """
+    return self.jwks_client.get_signing_key_from_jwt(token)
